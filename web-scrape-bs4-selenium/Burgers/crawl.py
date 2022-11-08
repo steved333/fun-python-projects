141d6be8beb5c0c820d1fcf7e6c64e6a4e6076d3
Steve D'Agostino <steved333@gmail.com>
2022-11-08 06:03:40 -0500

    First web scrape from McD Berlin

--- web-scrape-bs4-selenium/Burgers/crawl.py
+++ web-scrape-bs4-selenium/Burgers/crawl.py
@@ -6,8 +6,10 @@ import requests
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from time import sleep
+import csv
+import os
 
-# page =requests.get("https://www.mcdonalds.com/de/de-de/restaurant-suche.html/l/berlin")
+
 driver = webdriver.Chrome()
 driver.get("https://www.mcdonalds.com/de/de-de/restaurant-suche.html/l/berlin")
 sleep(10)
@@ -16,5 +18,25 @@ soup = BS(page, 'lxml')
 
 content = soup.find('ul', class_='ubsf_sitemap-list')
 restaurantsList = content.find_all('div', class_='ubsf_sitemap-location-address')
-print(restaurantsList)
+
+
+try:
+  os.mkdir("web-scrape-bs4-selenium/Burgers/CSV")
+except OSError as e:
+  print("Directory exists")
+
+
+with open('web-scrape-bs4-selenium/Burgers/CSV/restaurant.csv', mode='w', newline='') as outputFile:
+  restaurantCSV = csv.writer(outputFile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+  restaurantCSV.writerow((['restaurant', 'street', 'zip', 'city', 'country']))
+
+  restaurants = 'McDonalds'
+  city = 'Berlin'
+  country = 'Germany'
+
+  for restaurant in restaurantsList:
+    street = restaurant.text.split(',')[0]
+    zip = restaurant.text.split(',')[1:6]
+    restaurantCSV.writerow(([restaurants, street, zip, city, country]))
+
 driver.close()
